6172a79808b2e3dc3e2b2f08449edb44d6f61416
Pawel Kosiec <pawel.kosiec@gmail.com>
2018-11-13 17:14:20 +0100

    Prepare main docs page and version pages

--- src/api-extensions/documentation-pages.js
+++ src/api-extensions/documentation-pages.js
@@ -1,7 +1,9 @@
 const path = require("path");
 const compareVersions = require("compare-versions");
+const ui = require("../locales/en/UI.json");
 const DocsLoader = require("./DocsLoader");
 
+// FIXME: Do not use  GraphQL, use Node.js instead
 async function getDocsVersions(graphql) {
   const result = await graphql(`
     {
@@ -36,6 +38,30 @@ async function createDocsPages({ graphql, createPage }) {
     return;
   }
 
+  // Main /docs page
+  const latestVersion = versions[0];
+
+  const loader = new DocsLoader(latestVersion);
+  const navigation = loader.loadNavigation();
+  const manifest = loader.loadManifest();
+
+  //TODO: Refactor
+  // FIXME: not root, just first key + first item id
+  const latestVersionContent = loader.loadContent(
+    "root",
+    manifest.spec.root.id,
+  );
+  createPage({
+    path: `/docs`,
+    component: template,
+    context: {
+      displayName: ui.navigation.documentation,
+      content: latestVersionContent,
+      navigation,
+      manifest,
+    },
+  });
+
   versions.forEach(version => {
     const loader = new DocsLoader(version);
 
@@ -46,6 +72,23 @@ async function createDocsPages({ graphql, createPage }) {
       console.error(`Incorrect manifest for version ${version}`);
       return;
     }
+
+    // main version page
+
+    // FIXME: not root, just first key + first item id
+    const versionContent = loader.loadContent("root", manifest.spec.root.id);
+    createPage({
+      path: `/docs/${version}`,
+      component: template,
+      context: {
+        displayName: `${version} - ${ui.navigation.documentation}`,
+        content: versionContent,
+        navigation,
+        manifest,
+      },
+    });
+
+    // subpages
 
     Object.keys(manifest.spec).forEach(contentType => {
       const obj = manifest.spec[contentType];
@@ -58,7 +101,7 @@ async function createDocsPages({ graphql, createPage }) {
           path: `/docs/${version}/${contentType}/${page.id}`,
           component: template,
           context: {
-            displayName: page.displayName,
+            displayName: `${page.displayName} - ${ui.navigation.documentation}`,
             content,
             navigation,
           },
